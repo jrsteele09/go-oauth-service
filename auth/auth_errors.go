43052eab099b35167045fc39d9b84778731ada99
jrsteele09 <xsteelej72@hotmail.com>
2025-11-25 17:59:38 +0000

    refactored errors

--- auth/auth_errors.go
+++ auth/auth_errors.go
@@ -3,11 +3,11 @@ package auth
 import "errors"
 
 var (
-	InvalidClientIDErr        = errors.New("invalid client id")
-	InvalidTenantErr          = errors.New("invalid tenant")
-	InvalidAccessTokenErr     = errors.New("invalid access token")
-	UserNotFoundErr           = errors.New("user not found")
-	UserBlockedErr            = errors.New("user blocked")
-	UserUnverifiedErr         = errors.New("user not verified")
-	UserPasswordsDontMatchErr = errors.New("user passwords not matched")
+	ErrInvalidClientID        = errors.New("invalid client id")
+	ErrInvalidTenant          = errors.New("invalid tenant")
+	ErrInvalidAccessToken     = errors.New("invalid access token")
+	ErrUserNotFound           = errors.New("user not found")
+	ErrUserBlocked            = errors.New("user blocked")
+	ErrUserUnverified         = errors.New("user not verified")
+	ErrUserPasswordsDontMatch = errors.New("user passwords not matched")
 )
